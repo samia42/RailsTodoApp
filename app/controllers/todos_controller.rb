1517cb06809af39f62e5151a5665b311a0f86d25
samia42 <samiasaif42@gmail.com>
2022-08-03 16:20:01 +0500

    add edit action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,21 +5,43 @@ class TodosController < ApplicationController
     end
     def create
       @todo = Todo.new(todo_params)
-      respond_to do |format|
+     
       if @todo.save
+        respond_to do |format|
         format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        end
         # format.json { render :show, status: :created, location: @todo }
         # render :action => 'show', :id => @todo.id
         # redirect_to action: "show", id: @todo
       else
           render :new, status: :unprocessable_entity
       end
-    end
+    
     end
 
     def show
         @todo= Todo.find(params[:id])
     end
+
+    def edit
+        @todo= Todo.find(params[:id])
+    end
+
+    def update
+        @todo= Todo.find(params[:id])
+     
+        if  @todo.update(todo_params)
+            respond_to do |format|
+            format.html { redirect_to @todo, notice: 'Todo was updated successfully ' }
+            end
+        else
+           
+            render :edit , status: :unprocessable_entity
+        end
+      
+    end
+
+
 
     private
     def todo_params
